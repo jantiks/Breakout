4c50a4a435e114534f6d5256a66896578372cf69
jantiks <58886410+jantiks@users.noreply.github.com>
2021-01-26 16:50:23 +0400

    added paddle

--- src/Breakout.java
+++ src/Breakout.java
@@ -5,6 +5,7 @@ import acm.program.*;
 import acm.graphics.*;
 
 import java.awt.*;
+import java.awt.event.*;
 
 public class Breakout extends GraphicsProgram {
     /** Width and height of application window in pixels */
@@ -46,27 +47,78 @@ public class Breakout extends GraphicsProgram {
 
     /** Number of turns */
     private static final int NTURNS = 3;
-
+    // runs the program
     public void run() {
         setup();
     }
-
+    // initial setup
     private void setup() {
         makeBricks();
+        makePaddle();
+        addMouseListeners();
     }
+    // makeing paddle
+    private void makePaddle() {
+        paddle = new GRect((WIDTH / 2) - (PADDLE_WIDTH / 2),HEIGHT - PADDLE_Y_OFFSET, PADDLE_WIDTH, PADDLE_HEIGHT);
+        paddle.setFilled(true);
+        add(paddle);
+    }
+
+    // makes bricks
     private void makeBricks() {
-        
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+        Color color;
+        for (int i = 0; i < NBRICK_ROWS; i++) {
+            // changing color of each 2 rows
+            if (i == 0 || i == 1) {
+                color = Color.RED;
+            } else if (i == 2 || i == 3) {
+                color = Color.ORANGE;
+            } else if (i == 4 || i == 5) {
+                color = Color.YELLOW;
+            } else if (i == 6 || i == 7) {
+                color = Color.GREEN;
+            } else {
+                color = Color.BLUE;
+            }
+            // making all bricks
+            for (int j = 0; j < NBRICKS_PER_ROW; j++) {
                 // x position of brick
                 int x = j * (BRICK_WIDTH + BRICK_SEP);
                 // y position of brick
                 int y = i * (BRICK_HEIGHT + BRICK_SEP) + BRICK_Y_OFFSET;
 
-                //making brick
+                //making a brick
                 GRect brick = new GRect(x , y, BRICK_WIDTH, BRICK_HEIGHT);
+                // setting color to brick
+                brick.setFilled(true);
+                brick.setColor(color);
+                // adding to canvas
                 add(brick);
             }
         }
     }
+    // Called on mouse press to record the coordinates of the click */
+    public void mousePressed(MouseEvent mouseEvent) {
+        if (paddle.contains(mouseEvent.getX(), mouseEvent.getY())) {
+            currentPosition = new GPoint(mouseEvent.getPoint());
+        } else {
+            currentPosition = null;
+        }
+    }
+    // Called on mouse drag to reposition the object
+    public void mouseDragged(MouseEvent mouseEvent) {
+        if (currentPosition != null) {
+            if (currentPosition.getX() > (WIDTH - PADDLE_WIDTH)) {
+                currentPosition = new GPoint(paddle.getX() + PADDLE_WIDTH, paddle.getY());
+            } else {
+                paddle.move(mouseEvent.getX() - currentPosition.getX(), 0);
+                currentPosition = new GPoint(mouseEvent.getPoint());
+            }
+
+        }
+    }
+
+    // instance variables
+    private GRect paddle;
+    private GPoint currentPosition;
 }
